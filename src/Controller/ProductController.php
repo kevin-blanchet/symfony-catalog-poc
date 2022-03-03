376234fa270fe40f7c183aef17f0cd8c509b6a80
kevin-blanchet <kevin.blanchet@ynov.com>
2022-03-03 19:47:56 +0100

    Update ProductController.php
    moved role check to annotations

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,10 +23,9 @@ class ProductController extends AbstractController
     }
 
     #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_PRODUCT_ADMIN')]
     public function new(Request $request, ProductRepository $productRepository): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_PRODUCT_ADMIN');
-
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
@@ -50,10 +50,9 @@ class ProductController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_PRODUCT_ADMIN')]
     public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_PRODUCT_ADMIN');
-
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
@@ -69,10 +68,9 @@ class ProductController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_product_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_PRODUCT_ADMIN')]
     public function delete(Request $request, Product $product, ProductRepository $productRepository): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_PRODUCT_ADMIN');
-
         if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
             $productRepository->remove($product);
         }
